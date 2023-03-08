8a0d78f8895659aad2dbe9b532e539eb7410ba36
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-03-08 20:17:37 +0100

    Started crate About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <p>{title}</p>
+  )
+}
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <motion.div variants={textVariant()}>
+      <p className={styles.sectionSubText}>
+        Introduction</p>
+      <h2 className={styles.sectionHeadText}>
+        Overview.</h2>
+    </motion.div>
+
+    <motion.p
+    variants={fadeIn("", "", 0.1, 1)}
+    className="m4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+      I'm interested in Web technologies and I love make webstite.
+    </motion.p>
+
+    <div className="mt-20 flex flex-wrap gap-10">
+      {services.map((service, index) => (
+        <ServiceCard key={service.title} index={index} {...service} />
+      ))}
+    </div>
+    </>
   )
 }
 
