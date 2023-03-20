930823e23050fad3bfb8044f1a3b2c8130761036
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-03-20 13:39:36 +0100

    Fixed framer motion error

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Tilt from "react-tilt";
+import Tilt from "react-parallax-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -12,8 +12,7 @@ const ServiceCard = ({ index, title, icon }) => {
     <Tilt className="xs:w-[250px] w-full">
       <motion.div
        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
-       className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
-       >
+       className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card">
         <div
          options={{
           max: 45,
